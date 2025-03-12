2632ade3e112327f41a96f218eef5594b71feac5
tsm13 <thais@live.jp>
2025-03-12 20:37:29 -0300

    api fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,27 +1,15 @@
 import { createClient } from "@/app/_lib/supabase-server";
 import { NextResponse } from "next/server";
-// The client you created from the Server-Side Auth instructions
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${process.env.ROOT_URL}/account`);
-        // } else if (forwardedHost) {
-        //   return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${process.env.ROOT_URL}/account`);
-      }
+      return NextResponse.redirect(`${process.env.ROOT_URL}/account`);
     }
   }
 
